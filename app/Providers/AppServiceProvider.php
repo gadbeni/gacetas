32b219b7f4f5ef84cf4e3e19dee026eaf00a63d0
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-08-05 09:16:57 -0400

    docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,7 +30,7 @@ class AppServiceProvider extends ServiceProvider
             URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS', true);
         }
-        
+
         Paginator::useBootstrap();
     }
 }
